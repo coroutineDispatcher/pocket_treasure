caedbd9cd46df68e242cb8323068116f2e565697
unknown <sxhardha@ikons-see.com>
2019-09-04 17:11:10 +0200

    Getting app ready for dark mode

--- app/src/main/java/com/stavro_xhardha/PocketTreasureApplication.kt
+++ app/src/main/java/com/stavro_xhardha/PocketTreasureApplication.kt
@@ -1,6 +1,9 @@
 package com.stavro_xhardha
 
 import android.app.Application
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import com.stavro_xhardha.pockettreasure.dependency_injection.DaggerPocketTreasureComponent
 import com.stavro_xhardha.pockettreasure.dependency_injection.PocketTreasureComponent
 import net.danlew.android.joda.JodaTimeAndroid
@@ -12,6 +15,10 @@ class PocketTreasureApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         JodaTimeAndroid.init(this)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        if (AppCompatDelegate.getDefaultNightMode() == MODE_NIGHT_YES) {
+            Log.d("YES", "YES")
+        }
         pocketTreasureComponent = DaggerPocketTreasureComponent.factory().create(this)
         INSTANCE = pocketTreasureComponent
     }
