508f1a1b07160bc365ceae2396ef9dd0abbafe0f
stavro96 <stavro96@gmail.com>
2019-11-03 14:01:54 +0100

    found a way to sort dependencies out

--- core_module/src/main/java/com/stavro_xhardha/core_module/dependency_injection/modules/NetworkModule.kt
+++ core_module/src/main/java/com/stavro_xhardha/core_module/dependency_injection/modules/NetworkModule.kt
@@ -1,26 +1,14 @@
 package com.stavro_xhardha.core_module.dependency_injection.modules
 
-import com.stavro_xhardha.core_module.brain.PRAYER_API_BASE_URL
 import com.stavro_xhardha.core_module.core_dependencies.TreasureApi
 import dagger.Module
 import dagger.Provides
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 
-@Module(includes = [InterceptorModule::class])
+@Module(includes = [BaseNetworkModule::class])
 object NetworkModule {
-
-    @JvmStatic
-    @Provides
-    @Singleton
-    fun provideRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
-        .baseUrl(PRAYER_API_BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create())
-        .client(client)
-        .build()
 
     @JvmStatic
     @Provides
--- names_module/src/main/java/com/sxhardha/names_module/di/NamesNetworkModule.kt
+++ names_module/src/main/java/com/sxhardha/names_module/di/NamesNetworkModule.kt
@@ -1,12 +1,13 @@
 package com.sxhardha.names_module.di
 
 import com.stavro_xhardha.core_module.dependency_injection.FragmentScoped
+import com.stavro_xhardha.core_module.dependency_injection.modules.BaseNetworkModule
 import com.sxhardha.names_module.network.NamesApi
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 
-@Module
+@Module(includes = [BaseNetworkModule::class])
 object NamesNetworkModule {
 
     @JvmStatic
